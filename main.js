be08c352f8a8445c9962d243b2498962e699af60
kingpower31 <113869465+kingpower31@users.noreply.github.com>
2023-06-05 15:47:51 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const inputs = Array.from(document.querySelectorAll("input"));
+const btn = document.querySelector("button");
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", handleInput);
+});
+
+function handleInput(e) {
+  if (inputs.some((input) => input.value == "")) {
+    btn.disabled = true;
+  } else {
+    btn.disabled = false;
+  }
+}
